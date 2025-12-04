2d26a0d156654c4393ba386ebc504cf8f40ab540
nicole <njl55@cornell.edu>
2025-12-04 13:38:03 -0500

    feat: implemented schemas.py for Pydantic schemas

---
+++ app/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field, field_validator
+from decimal import Decimal
+
+class VehicleBase(BaseModel):
+  # FastAPI built in excpetion handlers handle 422 and 400
+  # exclude VIN... should not be updatable
+  manufacturer_name: str
+  description: str | None = None
+  horse_power: int
+  model_name: str
+  model_year: int
+  purchase_price: Decimal
+  fuel_type: str
+
+
+class VehicleCreate(VehicleBase):
+  # adds VIN and makes sure it is lowercase
+  vin: str = Field(..., min_length=1, max_length=17)
+  
+  @field_validator("vin")
+  @classmethod
+  def vin_validate(cls, v):
+    return v.lower() if v else v
+    
+
+class VehicleUpdate(VehicleBase):
+    """
+    PUT: requires a full vehicle object, all fields are required
+    VIN cannot be updated.
+    """
+    # requires all fields, just inherit from the VehicleBase
+    pass
+
+  
+############################
+#    RESPONSE MODEL        #
+############################
+class VehicleRead(VehicleBase):
+  vin: str
+
+  #
+  class Config:
+    from_attributes = True
